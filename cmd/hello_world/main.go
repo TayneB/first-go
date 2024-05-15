a609ad18d4deb92f02869ec57d3bea2b2685502a
Tayne Bilyard <taynebilyard@gmail.com>
2024-05-15 18:26:54 +1200

    Divider

--- cmd/hello_world/main.go
+++ cmd/hello_world/main.go
@@ -5,8 +5,15 @@ import "fmt"
 func main() {
 	printable := "Hello World"
 	printed(printable)
+	fmt.Println(intDivider(10, 3))
 }
 
 func printed(printValue string) {
 	fmt.Println(printValue)
+}
+
+func intDivider(numerator int, denominator int) (int, int) {
+	var result int = numerator / denominator
+	var remainder int = numerator % denominator
+	return result, remainder
 }
